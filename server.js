73d8ba0a78d11502d809bb48bcf08e04552486cb
Wenlong Jiang <jwlbjtu@gmail.com>
2020-04-27 16:02:29 -0400

    add comments

--- server.js
+++ server.js
@@ -49,8 +49,9 @@ app.get('/data', (req, res) => {
 // React Routing
 if(process.env.NODE_ENV === "production") {
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
+    // Serve any static files
     app.use(express.static(path.join(__dirname, 'client/build')));
-
+    // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
