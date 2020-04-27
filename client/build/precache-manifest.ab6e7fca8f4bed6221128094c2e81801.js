c5077f41efdb9601eec1269de15e2132357da1b5
Wenlong Jiang <jwlbjtu@gmail.com>
2020-04-27 00:42:31 -0400

    add build

---
+++ client/build/precache-manifest.ab6e7fca8f4bed6221128094c2e81801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579ffd7ce65f9cca2ac961bbc84cf508",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b24d45bf9e961cc5b2a2",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e22d6256e483263fa4b8",
+    "url": "/static/js/2.28c7256f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.28c7256f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b24d45bf9e961cc5b2a2",
+    "url": "/static/js/main.5f8411d7.chunk.js"
+  },
+  {
+    "revision": "b5da41a4f09a10b25f0b",
+    "url": "/static/js/runtime-main.7ca5be63.js"
+  }
+]);
